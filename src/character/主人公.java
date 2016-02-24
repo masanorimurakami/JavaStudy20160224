5654d867c210f456fab29cf2ffe4e6fcbe4a00da
masanorimurakami <masanori.murakami.v@gmail.com>
2016-02-24 20:44:42 +0900

    コンストラクタの追記

--- src/character/主人公.java
+++ src/character/主人公.java
@@ -10,13 +10,14 @@ package character;
  */
 public class 主人公 {
 
-	private int 最大体力=0;
-	private int 今の体力=0;
-	private int 攻撃力 = 0;
-	private int 素早さ = 0;
-	private int 防御力 = 0;
-	private int 経験値 = 0;
-	private int レベル = 0;
+	// 主人公のパラメータ
+	private double 最大体力 = 0;
+	private double 今の体力 = 0;
+	private double 攻撃力 = 0;
+	private double 素早さ = 0;
+	private double 防御力 = 0;
+	private double 経験値 = 0;
+	private double レベル = 0;
 	private String 名前 = "";
 
 	/** クラス生成時に呼び出されるメソッド
@@ -30,13 +31,13 @@ public class 主人公 {
 		// 名前をセット
 		名前 = _名前;
 		// 保存された経験値情報の取得
-		経験値 = 10;					// ダミー実装
-			// ファイルからレコードを読み込む
-			// 同じ名前があれば、経験値を取り込む
+		経験値 = 3500; // ダミー実装
+		// ファイルからレコードを読み込む
+		// 同じ名前があれば、経験値を取り込む
 		// 経験値からレベルを取得
 		レベル = getレベル(経験値);
 		// 保存された体力情報を取得
-		今の体力 = 10;						// ダミー実装
+		今の体力 = 10; // ダミー実装
 		// 最大体力をセット
 		最大体力 = get最大体力(レベル);
 		// 攻撃力をセット
@@ -47,6 +48,26 @@ public class 主人公 {
 		素早さ = get素早さ(レベル);
 	}
 
+	private double get素早さ(double _レベル) {
+		// TODO 自動生成されたメソッド・スタブ
+		return 0.010203041 * Math.pow(_レベル, 2) + 10;
+	}
+
+	private double get防御力(double _レベル) {
+		// TODO 自動生成されたメソッド・スタブ
+		return 0.051015203 * Math.pow(_レベル, 2) + 10;
+	}
+
+	private double get攻撃力(double _レベル) {
+		// TODO 自動生成されたメソッド・スタブ
+		return 0.071421284 * Math.pow(_レベル, 2) + 10;
+	}
+
+	private double get最大体力(double _レベル) {
+		// 最大体力を計算する。
+		return 0.101928375 * Math.pow(_レベル, 2) + 10;
+	}
+
 	/**
 	 * 経験値からレベルをセットする。
 	 *
@@ -54,9 +75,9 @@ public class 主人公 {
 	 * @param _経験値
 	 * @return
 	 */
-	private int getレベル(int _経験値) {
+	private double getレベル(double _経験値) {
 		// 経験値からレベルをセットする。
-		return 0;
+		return Math.sqrt(_経験値/51.01520253);
 	}
 
 	/**
@@ -67,8 +88,72 @@ public class 主人公 {
 	 * 保存してほしいユーザーデータの文字列を生成するだけにした方が良い
 	 *
 	 */
-	public String 保存(){
+	public String 保存() {
 		return null;
+	}
+
+	public double get最大体力() {
+		return 最大体力;
+	}
+
+	public void set最大体力(double 最大体力) {
+		this.最大体力 = 最大体力;
+	}
+
+	public double get今の体力() {
+		return 今の体力;
+	}
+
+	public void set今の体力(double 今の体力) {
+		this.今の体力 = 今の体力;
+	}
+
+	public double get攻撃力() {
+		return 攻撃力;
+	}
+
+	public void set攻撃力(double 攻撃力) {
+		this.攻撃力 = 攻撃力;
+	}
+
+	public double get素早さ() {
+		return 素早さ;
+	}
+
+	public void set素早さ(double 素早さ) {
+		this.素早さ = 素早さ;
+	}
+
+	public double get防御力() {
+		return 防御力;
+	}
+
+	public void set防御力(double 防御力) {
+		this.防御力 = 防御力;
+	}
+
+	public double get経験値() {
+		return 経験値;
+	}
+
+	public void set経験値(double 経験値) {
+		this.経験値 = 経験値;
+	}
+
+	public double getレベル() {
+		return レベル;
+	}
+
+	public void setレベル(double レベル) {
+		this.レベル = レベル;
+	}
+
+	public String get名前() {
+		return 名前;
+	}
+
+	public void set名前(String 名前) {
+		this.名前 = 名前;
 	}
 
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,25 @@
 package main;
 
+import character.主人公;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		// 主人公の生成
 
+		主人公 松浦 = new 主人公("松浦");
+		主人公 村上 = new 主人公("村上");
+
+		String バトル結果 = バトル(松浦,村上);
+		System.out.println(" " + バトル結果);
+
+//		System.out.println(勇者.get名前() + "のレベルは、" + String.valueOf(勇者.getレベル()));
+
+	}
+
+	private static String バトル(主人公 _松浦, 主人公 _村上) {
+		// TODO 自動生成されたメソッド・スタブ
+		return _松浦.get名前() + "の勝ち。";
 	}
 
 }
